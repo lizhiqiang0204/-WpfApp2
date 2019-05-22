1c0cf658d7d30b0c76c4a7fd4f41a087e26dc0da
lizhiqiang0204 <18518092079@163.com>
2019-05-22 10:21:18 +0800

    first commit

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Calculate
+    {
+        public int sum(int a, int b)//加
+        {
+            return a + b;
+        }
+
+        public int sub(int a, int b)//减
+        {
+            return a + b;
+        }
+    }
+}
---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using ClassLibrary1;//应用自己创建的类库
+using System.Windows;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Calculate calculate = new Calculate();//实例化类库里的类
+        int sum = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            sum = calculate.sum(1, 2);
+            MessageBox.Show("算术和为：" + sum.ToString());
+        }
+    }
+}
